1b32d134ce8ba3854ec0089e69b023c2bbe2a0a6
ronan-gloo <sonotone13@gmail.com>
2011-09-05 22:13:11 +0200

    anonymous function for bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -11,46 +11,56 @@ Autoloader::add_core_namespace('Helper');
 * class Date extends DateHelper {}
 *
 */
-require_once(__DIR__.'/config/bootstrap.php');
 
 // convert class to procedural if asked
-$boot = null;
-if ($helper_bt['convert_to_procedural'] === true)
-{
-	$boot = function($class) use($helper_bt) {
-		$methods = get_class_methods($class);
-		foreach ($methods as $method)
-		{
-			if (! in_array($method, $helper_bt['procedural_blacklist'][strtolower($class)]))
+call_user_func(function(){
+	
+	include_once  __DIR__.'/config/bootstrap.php';
+	
+	$boot = null;
+	
+	if ($helper_bt['convert_to_procedural'] === true)
+	{
+		$boot = function($class) use($helper_bt) {
+			
+			$methods = get_class_methods($class);
+			
+			foreach ($methods as $method)
 			{
-				$alias = strtolower($class).'_'.$method;
-		  	if (! function_exists($alias))
+				if (! in_array($method, $helper_bt['procedural_blacklist'][strtolower($class)]))
 				{
-					$command = "function ".$alias."() { "; 
-					$command.= "\$args = func_get_args(); ";
-					$command.= "return call_user_func_array(array(\"".$class."\", \"".$method."\"), \$args); }"; 
-					@eval($command);
+					$alias = strtolower($class).'_'.$method;
+			  	
+			  	if (! function_exists($alias))
+					{
+						$command = "function ".$alias."() { "; 
+						$command.= "\$args = func_get_args(); ";
+						$command.= "return call_user_func_array(array(\"".$class."\", \"".$method."\"), \$args); }"; 
+						@eval($command);
+					}
 				}
 			}
+		};
+	}
+	
+	// init namespaces and class names
+	$nspace = 'Helper\\';
+	
+	foreach ($helper_bt['classes'] as $key => $class)
+	{
+		$cpath = $helper_bt['override_folder'].strtolower($class).'.php';
+		if ( ! file_exists($cpath) OR ! is_file($cpath))
+		{
+			class_alias($nspace.$key, $class);
 		}
-	};
-}
-
-// init namespaces and class names
-$nspace = 'Helper\\';
-foreach ($helper_bt['classes'] as $key => $class)
-{
-	$cpath = $helper_bt['override_folder'].strtolower($class).'.php';
-	if ( ! file_exists($cpath) OR ! is_file($cpath))
-	{
-		class_alias($nspace.$key, $class);
+		else
+		{
+			Autoloader::add_class($nspace.$class, $cpath);
+		}
+		
+		if ($boot) $boot($class);
 	}
-	else
-	{
-		Autoloader::add_class($nspace.$class, $cpath);
-	}
-	if ($boot) $boot($class);
-}
-$boot = null;
+	
+	$boot = null;
 
-/* End of file bootstrap.php */
+});
